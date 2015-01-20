e2b0d94a559e76b513ced3f5c7941e4e27ffe76f
LaputaPrince <denzeljiang@gmail.com>
2015-01-19 23:08:16 -0500

    prefer ibus in crouton

--- .config/awesome/hal.lua
+++
@@ -1,9 +0,0 @@
-hardware = {}
-
-hardware.net = "wlan0"
-
-hardware.keyVolumeUp = "XF86AudioRaiseVolume"
-hardware.keyVolumeDown = "XF86AudioLowerVolume"
-hardware.keyVolumeMute = "XF86AudioMute"
-
-return hardware
